3aa15c922babf2896a780184207aa3b1982709c3
henry hace tarea <henry hace tarea@SERVIDOR>
2017-04-13 18:46:29 -0400

    Registro de empleados Modelo, Registro de empleados controlador, Registro de empleados vista terminados en su primera version, estara totalmente terminada al tener la base de datos

---
+++ Importadora/src/application/RegistroEmpleadoController.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author henry hace tarea
+ */
+public class RegistroEmpleadoController {
+    @FXML
+    private Label titulo;
+    //Labels usados para mostrar que va en cada casilla 
+    @FXML
+    private Label titNombre;
+    @FXML
+    private Label titCi;
+    @FXML
+    private Label titTelefono;
+    @FXML
+    private Label titDireccion;
+    //CAMPOS DE TEXTO
+    @FXML
+    private TextField tfNombre;
+    @FXML
+    private TextField tfCi;
+    @FXML
+    private TextField tfTelefono;
+    @FXML
+    private TextField tfDireccion;
+    //Botones
+    @FXML
+    private Button btnRegistrar;
+    
+    @FXML
+    private void registrar(ActionEvent e){
+        JOptionPane.showMessageDialog(null, "REGISTRADO");
+    }
+}
