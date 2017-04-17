cadc3cb0402210674b0656696395d9fcc46d4ac5
Mauricio Cadima Guevara <guevara0411@gmail.com>
2017-04-16 22:22:51 -0400

    Se implemento la ventana de buscar paises,se modifico para que la ventana no pueda cambiar de tamanho

--- sis2/sis2/src/buscarpaises/VentanasBuscar.java
+++ sis2/sis2/src/buscarpaises/VentanasBuscar.java
@@ -6,9 +6,12 @@ package buscarpaises;
  * and open the template in the editor.
  */
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -21,27 +24,15 @@ import javafx.stage.Stage;
 public class VentanasBuscar extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+    public void start(Stage primaryStage) throws IOException {
+        Parent root=FXMLLoader.load(getClass().getResource("VentanaBuscar.fxml"));
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+        Scene scene=new Scene(root);
         primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
-
+    
     /**
      * @param args the command line arguments
      */
