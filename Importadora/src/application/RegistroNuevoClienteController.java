76816a80365d5a45279108a557f7f455900d6d91
Nicolas <nlrc272@gmail.com>
2017-04-17 15:16:52 -0400

    actualizacion interfaz NuevoCliente

--- Importadora/src/application/RegistroNuevoClienteController.java
+++ Importadora/src/application/RegistroNuevoClienteController.java
@@ -1,50 +1,93 @@
 package application;
 
 
+import java.net.URL;
 import java.sql.ResultSet;
 import java.time.LocalDate;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import pkgControlador.ConectaDB;
 
-public class RegistroNuevoClienteController {
+public class RegistroNuevoClienteController implements Initializable{
         ConectaDB conector=new ConectaDB();
 	LocalDate fecha1;
 	@FXML
 	DatePicker fecha;
-        @FXML private TextField tfNombreCli;
-        @FXML private TextField tfApellidoCli;
-        @FXML private TextField tfCarnetCli;
-	@FXML public void a(ActionEvent ev){
-		String apellidoCli = tfApellidoCli.getText();
-                String nombreCli = tfNombreCli.getText();
-                String carnetCli = tfCarnetCli.getText();
+        @FXML private TextField txtNomcCliente;
+        @FXML private TextField txtApeCliente;
+        @FXML private TextField txtNitCi;
+        @FXML private TextField txtNomProducto;
+        @FXML private TextField txtCantPoducto;
+        @FXML private TextField txtCstProducto;
+        @FXML private DatePicker datePedido;
+        @FXML private DatePicker dateLLegada;
+        @FXML private TextField txtCstPedido;
+        @FXML private ComboBox comboProveedor=new ComboBox(llenarComboProveedor());
+	@FXML public void btnRegistroPedidoAction(ActionEvent ev){
+		String apellidoCli = txtApeCliente.getText();
+                String nombreCli = txtNomcCliente.getText();
+                int carnetCli = Integer.parseInt(txtNitCi.getText());
+                String nombreProducto=txtNomProducto.getText();
+                String cantidadProducto=txtCantPoducto.getText();
+                int productoCantidad=Integer.parseInt(cantidadProducto);
+                String costoProducto=txtCstProducto.getText();
+                int productoCosto=Integer.parseInt(costoProducto);
+                String nombreProveedor=comboProveedor.getValue().toString();
+                String fechaPedido=datePedido.getValue().toString();
+                String fechaLlegada=dateLLegada.getValue().toString();  
+                String costoPedido=txtCstPedido.getText();
+                int pedidoCosto=Integer.parseInt(costoPedido);
                 ResultSet res;
         
-        try {
-            res=conector.consultar("Select registrarcliente("+carnetCli+",'"+apellidoCli+"','"+nombreCli+"' );");
-            if (res.next()) {
-                String retorno=res.getString("registrarcliente");
-                if (retorno.equals("registrado")) {
+            try {
+                res=conector.consultar("select registrarPedido('"+nombreCli+"','"+apellidoCli+"',"+carnetCli+",'"+nombreProducto+"',"+productoCantidad+","+productoCosto+",'"+nombreProveedor+"','"+fechaPedido+"','"+fechaLlegada+"',"+pedidoCosto+");");
+                if (res.next()) {
+                    String retorno=res.getString("registrarcliente");
+                    if (retorno.equals("registrado")) {
  
-                    res=conector.consultar("Select registrarPedido("+carnetCli+",'"+apellidoCli+"','"+nombreCli+"' );");
+                        
                                             
-                } else{
-                    System.out.println(retorno);
-                }
+                    } else{
+                        System.out.println(retorno);
+                    }
                 
-            } else {
-                System.err.println("Error");
+                } else {
+                    System.err.println("Error");
+                }
+            } catch (Exception e) {
+                System.err.println("Error : "+e);
             }
-        } catch (Exception e) {
-            System.err.println("Error : "+e);
-        }
 		
 	}
+        private ObservableList<String> llenarComboProveedor() { 
+
+            ObservableList<String> items=FXCollections.observableArrayList();
+            try {    
+                ResultSet rs=null; 
+                rs = conector.consultar("SELECT proveedor.nombre_prov FROM proveedor");
+                System.out.println("104");
+                while (rs.next()) {
+                    items.add(rs.getString(1));
+                    System.err.println(rs.getString(1) +"**");
+                }
+        
+            } catch(Exception err){err.printStackTrace();} 
+            return items;
+        }
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+        comboProveedor.setItems(llenarComboProveedor());
+    } 
 }
